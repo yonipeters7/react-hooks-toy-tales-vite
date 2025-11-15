8907edf1e84adc2b1c4eec6520be5a814292c95a
Yonatan Peters <yonipeters7@gmail.com>
2025-11-15 17:52:42 -0500

    Add full CRUD functionality for toys

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy, onDeleteToy, onUpdateToy }) {
+  function handleDelete() {
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => onDeleteToy(toy.id));
+  }
+
+  function handleLike() {
+    const updatedToy = {
+      ...toy,
+      likes: toy.likes + 1,
+    };
+
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedToy),
+    })
+      .then((response) => response.json())
+      .then((data) => onUpdateToy(data));
+  }
+
   return (
     <div className="card" data-testid="toy-card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{toy.name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={toy.image}
+        alt={toy.name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{toy.likes} Likes </p>
+      <button className="like-btn" onClick={handleLike}>Like {"<3"}</button>
+      <button className="del-btn" onClick={handleDelete}>Donate to GoodWill</button>
     </div>
   );
 }
